bbcf6d04fdcc476d7da1663926267e53b0e0a769
mmukhanov <mmukhanov@apple.com>
2020-12-08 10:35:40 -0800

    Smallest sufficient team

--- src/main/java/com/max/practice/excercise/SmallestSufficientTeam.java
+++ src/main/java/com/max/practice/excercise/SmallestSufficientTeam.java
@@ -4,51 +4,48 @@ import java.util.*;
 
 //https://leetcode.com/problems/smallest-sufficient-team/
 public class SmallestSufficientTeam {
-    List<Integer> sol = new ArrayList<>();
 
     public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
 
-        Map<String, Integer> idx = new HashMap<>();
-        int n = 0;
-        for (String skill : req_skills) idx.put(skill, n++);
-        int[] pe = new int[people.size()];
-        for (int i = 0; i < pe.length; i++) {
-            for (String skill : people.get(i)) {
-                int skillInx = idx.get(skill);
-                pe[i] += 1 << skillInx;
+        Map<String, Integer> skillIndex = new HashMap<>();
+        for (int i = 0; i < req_skills.length; i++) skillIndex.put(req_skills[i], i);
+
+        int[] peopleSkills = new int[people.size()];
+        for (int i = 0; i < peopleSkills.length; i++) {
+            for (String personSkill : people.get(i)) {
+                int skillIdx = skillIndex.get(personSkill);
+                peopleSkills[i] |= 1 << skillIdx;
             }
         }
 
-        search(0, pe, new ArrayList<>(), n);
-        int[] res = new int[sol.size()];
-        for (int i = 0; i < sol.size(); i++) res[i] = sol.get(i);
+        List<Integer> ans = new ArrayList<>();
+        search(0, new ArrayList<>(), peopleSkills, req_skills.length, ans);
+
+        int[] res = new int[ans.size()];
+        for (int i = 0; i < res.length; i++) res[i] = ans.get(i);
         return res;
     }
 
-    private void search(int teamSkills, int[] pe, List<Integer> team, int n) {
-        if (teamSkills == (1 << n) - 1) {
-            if (sol.isEmpty() || sol.size() > team.size()) {
-                sol.clear();
-                sol.addAll(team);
+    void search(int teamSkills, List<Integer> team, int[] peopleSkills, int reqSkillSize, List<Integer> ans) {
+        if (teamSkills == (1 << reqSkillSize) - 1) {
+            if (ans.isEmpty() || ans.size() > team.size()) {
+                ans.clear();
+                ans.addAll(team);
             }
             return;
         }
 
-        if (!sol.isEmpty() && team.size() >= sol.size()) return; //current team size is bigger than we have so far
+        if (!ans.isEmpty() && team.size() > ans.size()) return;
 
-        int zeroBits = 0;
-        while (((teamSkills >> zeroBits) & 1) == 1) zeroBits++;
+        int zeroBit = 0;
+        while (((teamSkills >> zeroBit) & 1) == 1) zeroBit++;
 
-        for (int i = 0; i < pe.length; i++) {
-            int personSkills = pe[i];
-            if (((personSkills >> zeroBits) & 1) == 1) {
+        for (int i = 0; i < peopleSkills.length; i++) {
+            if (((peopleSkills[i] >> zeroBit) & 1) == 1) {
                 team.add(i);
-                search(teamSkills | personSkills, pe, team, n);
+                search(teamSkills | peopleSkills[i], team, peopleSkills, reqSkillSize, ans);
                 team.remove(team.size() - 1);
             }
         }
-
     }
-
-
 }
