1b56e7ab0f9c1f60b960fd298d3b3f1152c71b14
mmukhanov <mmukhanov@apple.com>
2020-12-07 18:27:27 -0800

    Sudoku Solver

---
+++ src/main/java/com/max/practice/excercise/SudokuResolver.java
@@ -0,0 +1,43 @@
+package com.max.practice.excercise;
+
+public class SudokuResolver {
+    char EMPTY = '.';
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == EMPTY) {
+                    for(char value = '1'; value <= '9'; value++) {
+                        if (isValid(board, row, col, value)){
+                            board[row][col] = value;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = EMPTY;
+                            }
+                        }
+                    }
+                    return false;
+                }
+
+            }
+        }
+
+        return true;
+    }
+
+
+    private boolean isValid(char[][] board, int row, int col, char c){
+        for(int i = 0; i < 9; i++) {
+            if(board[i][col] != '.' && board[i][col] == c) return false; //check row
+            if(board[row][i] != '.' && board[row][i] == c) return false; //check column
+            if(board[3 * (row / 3) + i / 3][ 3 * (col / 3) + i % 3] != '.' &&
+                    board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false; //check 3*3 block
+        }
+        return true;
+    }
+}
+
